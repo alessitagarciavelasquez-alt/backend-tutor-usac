dafd0eb3304e4b6deee43c56d1762d0af97c87cc
alessitagarciavelasquez-alt <alessita.garcia.velasquez@gmail.com>
2026-04-14 16:51:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuración de Base de Datos para Render
-basedir = os.path.abspath(os.path.dirname(_file_))
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'estudiante_usac.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
